3c0d0d9d5e91bf7b6bef96d9e1289e6b8994d3b2
artuwap9 <41451099+artuwap9@users.noreply.github.com>
2019-05-05 13:55:32 +0100

    Delete loadSpecLabels.m

--- loadSpecLabels.m
+++
@@ -1,9 +0,0 @@
-function labels = loadSpecLabels()
-    images = [pwd,'/images/new_all/'];
-    jpgfiles = dir(fullfile(images,'*.jpg*'));
-    n = numel(jpgfiles);
-    for i = 1:n
-        name = jpgfiles(i).name;
-        labels(i) = str2double(name(length(name)-4));
-    end
-end
